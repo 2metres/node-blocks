ca7c6bb875f9bef55b403a49c2606564ee18079f
Andrew Carr <acarr@thoughtworks.com>
2013-08-04 23:32:20 +1000

    added waypoints.js to add a little magic while you scroll ✶ ✷ ✸

--- public/js/script.js
+++ public/js/script.js
@@ -10,5 +10,19 @@ $(window).load(function() {
     nextText: ">",  
     }
   );
+
+  $(".gallery").waypoint(function(){
+    $(this).removeClass('fade-it');
+  },{offset: '70%'});
+
   $("[data-toggle='tooltip']").tooltip();
+});
+
+
+$(window).scroll(function(){
+
+  $(".gallery").waypoint(function(){
+    $(this).removeClass('fade-it');
+  },{offset: '70%'});
+
 });
